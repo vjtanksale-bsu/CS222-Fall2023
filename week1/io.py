f2bee811b57ad70cc0c9810eb32bcacd13555cfe
VTANKSAL <vtanksal@csci-vtanksal-23.dhcp.bsu.edu>
2023-08-22 12:13:18 -0400

    for loop

--- week1/io.py
+++ week1/io.py
@@ -1,14 +1,26 @@
 def main():
   print("Welcome to CS 222")
-  firstName = input("Pleae enter your first name: ")
-  print("Welcome " + firstName)
-  payRate = float(input("Enter hourly pay rate: "))
-  hours = float(input("Enter number of hours worked: "))
-  print("$ " + str(payRate * hours))
+  #firstName = input("Please enter your first name: ")
+  #print("Welcome " + firstName)
+  #payRate = float(input("Enter hourly pay rate: "))
+  #hours = float(input("Enter number of hours worked: "))
+  #print("$ " + str(payRate * hours))
   average = float(input("Please enter student's average score: "))
   if average >= 60:
     print("Pass")
   else:
     print("Fail")
-  
+  if average >= 90:
+    print("A")
+  elif average >= 80:
+    print("B")
+  elif average >= 70:
+    print("C")
+  elif average >= 60:
+    print("D")
+  else:
+    print("F")
+
+  for counter in range(5):
+    print(counter)
 main()
