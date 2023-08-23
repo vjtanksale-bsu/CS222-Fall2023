840031d054718494d1a9d8956959e9bc5ffee3b1
Megan237 <megan.stinefield@bsu.edu>
2023-08-23 12:50:13 -0400

    5 for loop examples

---
+++ week1/For Loop/ForLoop3.py
@@ -0,0 +1,13 @@
+def main():
+    numbers = [7, 5, 19, 3]
+    for number in numbers:
+        print(number)
+
+    print()
+
+    total = 0
+    for number in numbers:
+        total += number
+    print(total)
+
+main()
