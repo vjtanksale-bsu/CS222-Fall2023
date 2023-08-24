08facd490fc1187607b2e9602a1af0eb54418350
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2023-08-24 12:11:18 -0400

    Week 1 Thursday

--- Loops/ForLoop4.py
+++ Loops/ForLoop4.py
@@ -1,5 +1,9 @@
 def main():
     numbers = [7, 5, 19, 3]
-    for i in range(0, len(numbers)):
+    for i in range(len(numbers)):
         print(numbers[i])
+    for x in range(5):
+        for y in range(3):
+            print("CS 222")
+
 main()
--- Loops/ForLoop5.py
+++ Loops/ForLoop5.py
@@ -3,4 +3,17 @@ def main():
         print(f"Outer loop {i}")
         for j in range(0, 3):
             print(f"Inner loop {j}")
+    for counter in range(10):
+        if (counter % 2 == 0):
+            print(f"Counter: {counter}")
+    afcSouth = ["Colts", "Titans", "Texans", "Jaguars"]
+    print(len(afcSouth))
+    print(len(afcSouth[0]))
+    print(afcSouth[3][2])
+    exams = [[75, 82, 93], [100, 92, 97], [50, 90, 80]]
+    for exam in exams:
+        for score in exam:
+            if not score >= 60:
+                print(score)
+
 main()
