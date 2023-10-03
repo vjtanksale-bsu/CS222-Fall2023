ddbbc4b654554a4f0eb06fbfed1b597eca0125b5
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2023-10-03 12:10:18 -0400

    Week 7 Teusday

---
+++ staticmethods.py
@@ -0,0 +1,12 @@
+class MathUtilities:
+    @staticmethod
+    def add(x, y):
+        return x + y
+    @staticmethod
+    def subtract(x, y):
+        return x - y
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+result = MathUtilities.add(5, 3)
+print(result)
