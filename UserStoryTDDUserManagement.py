508ea1085025d86ea81e77ee88b89b909cf4959a
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2023-11-14 12:07:38 -0500

    Quicksort

--- UserStoryTDDUserManagement.py
+++ UserStoryTDDUserManagement.py
@@ -6,6 +6,10 @@ using email and password
 User Story 2: Login
 As a registered user, I want to be able to log in to my account
 using email and password
+
+User Story 3: Password reset
+As a user who forgot their password, I want to be able to 
+request a password reset.
 '''
 
 import unittest
@@ -19,4 +23,14 @@ class TestUserManagement(unittest.TestCase):
         userManager = UserManagement()
         userManager.register("a@b.com", "password")
         result = userManager.login("a@b.com", "password")
+        self.assertTrue(result)
+    def test_password_reset_request(self):
+        userManager = UserManagement()
+        result = userManager.request_password_reset("a@b.com")
+        self.assertTrue(result)
+    def test_password_reset(self):
+        userManager = UserManagement()
+        userManager.register("a@b.com", "password")
+        reset_token = userManager.request_password_reset("a@b.com")
+        result = userManager.reset_password("a@b.com", reset_token, "newPassword")
         self.assertTrue(result)
